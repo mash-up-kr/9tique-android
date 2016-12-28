332b4e3fd27c145ecfe35bd7fc4e53df2a47a94e
DongHee Kim <opklnm102@gmail.com>
2016-12-29 03:19:07 +0900

    Refactor: 리소스 정리 및 테마 이름 수정

--- app/src/main/java/kr/co/mash_up/a9tique/ui/splash/SplashActivity.java
+++ app/src/main/java/kr/co/mash_up/a9tique/ui/splash/SplashActivity.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import kr.co.mash_up.a9tique.MainActivity;
+import kr.co.mash_up.a9tique.R;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -14,15 +15,12 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(i);
-
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
         }, SPLASH_TIME_OUT);
     }
 }
